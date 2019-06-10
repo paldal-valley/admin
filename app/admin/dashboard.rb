538000b674469538b5e2e959280fa6a98b8852b8
chiheon <qwaserdf234@naver.com>
2019-06-10 22:24:29 +0900

    [fix dashboard] 대시보드 수정

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -5,7 +5,7 @@ ActiveAdmin.register_page "Dashboard" do
     @users = User.all.count
     @posts = Post.all.count
     @comments = Comment.all.count
-    @question_accuracy = (PostsAnswer.where(isSelected: 1).count.to_f / PostsQuestion.all.count.to_f) * 100
+    @question_accuracy = ((PostsAnswer.where(isSelected: 1).count.to_f / PostsQuestion.all.count.to_f) * 100).round(2) 
     @recent_users = User.find_recent_week
     @recent_posts = Post.find_recent_week
     @weekly_posts = Post.ransack(createdDate_gteq: (Date.today - 6), createdDate_lteq: Date.today).result().count
@@ -13,6 +13,7 @@ ActiveAdmin.register_page "Dashboard" do
     @question_posts = PostsQuestion.ransack(createdDate_gteq: (Date.today - 6), createdDate_lteq: Date.today).result().count
     @answer_posts = PostsAnswer.ransack(createdDate_gteq: (Date.today - 6), createdDate_lteq: Date.today).result().count
     @review_posts = PostsReview.ransack(createdDate_gteq: (Date.today - 6), createdDate_lteq: Date.today).result().count
+    @hot_posts = Post.left_joins(:likes).group(:id).having('COUNT(likes.id) > 0').order('COUNT(likes.id) DESC')
   end
 
   content title: proc{ I18n.t("active_admin.dashboard") } do
